bdc7b06bc2ce97117e51640718ff45eaba285ab1
Darin Webster <darinleenavy@gmail.com>
2019-09-20 12:39:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(array)
-  n = 0
-    while i < array.length
-      yield(collection[n])
-      n = n + 1 
-    end
-  collection 
+  n = 0 
+  while n > array.length 
+    yield(array[n])
+    n = n + 1
+  end
+  array 
 end
