0fc831a79f45f2f853422495bde1df7c68726225
Darin Webster <darinleenavy@gmail.com>
2019-09-20 12:35:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  n = 0 
+  array.map do |i|
+    while i < array.length
+      yield[collection(i)]
+      i = i + 1 
+    end
+  end
+  collection 
 end
